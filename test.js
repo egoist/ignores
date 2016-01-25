d3b6186a3d16736a2bb602ed70896a3976a4cc8c
EGOIST <0x142857@gmail.com>
2016-01-26 00:38:31 +0800

    fix test

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ import ig from './'
 test('main', async t => {
 	const result = await ig(['node', 'osx'])
 	t.same(result[0].name, 'Node.gitignore')
-	t.same(result[1].name, 'global/OSX.gitignore')
+	t.same(result[1].name, 'Global/OSX.gitignore')
 	t.true(result[1].value.indexOf('.DS_Store') > -1)
 })
